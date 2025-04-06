1817b17e0b0a2d279fdc9c828a4b92b5db67deb4
Kishan Tiwari <tiwarikishan432004@gmail.com>
2025-04-06 14:24:20 +0530

    project is ready

--- app/dashboard/_components/Addnewinterview.jsx
+++ app/dashboard/_components/Addnewinterview.jsx
@@ -1,123 +1,165 @@
-"use client"
-import React, {useState} from 'react'
+"use client";
+import React, { useState } from "react";
+
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
-  } from "@/components/ui/dialog"
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
-import { Textarea } from '@/components/ui/textarea'
-import { chatSession } from '@/utils/GeminiAIModal'
-import { LoaderCircle } from 'lucide-react'
-import { db } from '@/utils/db'
-import { MockInterview } from '@/utils/schema'
-import { v4 as uuidv4 } from 'uuid';
-import { useUser } from '@clerk/nextjs'
-import moment from 'moment'
-import { useRouter } from 'next/navigation'
-  
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { chatSession } from "@/utils/GeminiAIModal";
+import { LoaderCircle } from "lucide-react";
+import { db } from "@/utils/db";
+import { MockInterview } from "@/utils/schema";
+import { v4 as uuidv4 } from "uuid";
+import { useUser } from "@clerk/nextjs";
+import moment from "moment";
+import { useRouter } from "next/navigation";
 
-function Addnewinterview() {
-    const [openDailog, setOpenDailog] = useState(false)
-    const [jobPosition,setJobPosition] = useState();
-    const [jobDesc,setJobDesc] = useState();
-    const [jobExperience,setJobExperience] = useState();
-    const [loading, setLoading] = useState(false);
-    const [JsonResponce, setJsonResponce] = useState([]);
-    const router = useRouter
-    const {user} = useUser();
+const AddNewInterview = () => {
+  const [openDailog, setOpenDialog] = useState(false);
+  const [jobPosition, setJobPosition] = useState();
+  const [jobDesc, setJobDesc] = useState();
+  const [jobExperience, setJobExperience] = useState();
+  const [loading, setLoading] = useState(false);
+  const [jsonResponse, setJsonResponse] = useState([]);
+  const { user } = useUser();
+  const router = useRouter();
 
-    const onSubmit = async(e)=>{
-        setLoading(true)
-        e.preventDefault()
-        console.log(jobPosition, jobDesc, jobExperience);
+  const onSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+    console.log(jobPosition, jobDesc, jobExperience);
 
-        const InputPrompt = "Job Position : "+jobPosition+", Job Description : "+jobDesc+", Years of Experience : "+jobExperience+", Depends on Job Position, Job Description & Years of Experience Give us "+process.env.NEXT_PUBLIC_INTERVIEW_QUESTION_COUNT+" Interview questions along with Answer in JSON format, Give us question and answer field on JSON"
+    const InputPrompt = `
+  Job Positions: ${jobPosition}, 
+  Job Description: ${jobDesc}, 
+  Years of Experience: ${jobExperience}. 
+  Based on this information, please provide 5 interview questions with answers in JSON format, ensuring "Question" and "Answer" are fields in the JSON.
+`;
 
-        const result = await chatSession.sendMessage(InputPrompt);
+    const result = await chatSession.sendMessage(InputPrompt);
+    const MockJsonResp = result.response
+      .text()
+      .replace("```json", "")
+      .replace("```", "")
+      .trim();
+    console.log(JSON.parse(MockJsonResp));
+    // const parsedResp = MockJsonResp
+    setJsonResponse(MockJsonResp);
 
-        const MockJsonResp = (result.response.text()).replace('```json', '').replace('```', '')
-
-        console.log(JSON.parse(MockJsonResp));
-        setJsonResponce(MockJsonResp);
-
-        if(MockJsonResp)
-        {
-
-        const resp = await db.insert(MockInterview)
+    if (MockJsonResp) {
+      const resp = await db
+        .insert(MockInterview)
         .values({
-          mockId:uuidv4(),
-          jsonMockResp:MockJsonResp,
-          jobPosition:jobPosition,
-          jobDesc:jobDesc,
-          jobExperience:jobExperience,
-          createdBy:user?.primaryEmailAddress?.emailAddress,
-          createdAt:moment().format('DD-MM-YYYY')
-        }).returning({mockId:MockInterview.mockId});
+          mockId: uuidv4(),
+          jsonMockResp: MockJsonResp,
+          jobPosition: jobPosition,
+          jobDesc: jobDesc,
+          jobExperience: jobExperience,
+          createdBy: user?.primaryEmailAddress?.emailAddress,
+          createdAt: moment().format("YYYY-MM-DD"),
+        })
+        .returning({ mockId: MockInterview.mockId });
 
-        console.log("Inserted ID:", resp)
-        if (resp)
-        {
-          setOpenDailog(false);
-          router.push('/dashboard/interview' +resp[0]?.mockId)
-        }
-        }
-        else{
-          console.log("ERROR");
-        }
-        setLoading(false);
+      console.log("Inserted ID:", resp);
+
+      if (resp) {
+        setOpenDialog(false);
+        router.push("/dashboard/interview/" + resp[0]?.mockId);
+      }
+    } else {
+      console.log("ERROR");
     }
+    setLoading(false);
+  };
+
   return (
     <div>
-      <div className='p-10 border rounded-lg bg-secondary hover:scale-105 hover:shadow-md cursor-pointer transition-all' onClick={() => setOpenDailog(true)}>
-        <h2 className='text-lg text-center'>+ Add New</h2>
+      <div
+        className="p-10 rounded-lg border bg-secondary hover:scale-105 hover:shadow-sm transition-all cursor-pointer hover:bg-gray-300"
+        onClick={() => setOpenDialog(true)}
+      >
+        <h2 className=" text-lg text-center">+ Add New</h2>
       </div>
       <Dialog open={openDailog}>
-      
-      <DialogContent className="max-w-2xl"> 
-          <DialogHeader> 
-          <DialogTitle className="text-2xl">Tell us more about your job Interview</DialogTitle> 
-          <DialogDescription> 
-            <form onSubmit={onSubmit}>
-              <div>
-                
-                <h2>Add Details about your job position/role, Job description and years of experience</h2>
+        <DialogContent className="max-w-2xl">
+          <DialogHeader>
+            <DialogTitle className="text-2xl">
+              Tell us more about your job interviwing
+            </DialogTitle>
+            <DialogDescription>
+              <form onSubmit={onSubmit}>
+                <div className="my-3">
+                  <h2>
+                    Add Details about your job position, job descritpion and
+                    years of experience
+                  </h2>
 
-                <div className='mt-7 my-3'>
-                    <label>Job Role/Job Position</label>
-                    <Input placeholder="Ex. Full Stack Developer" required onChange={(event)=>setJobPosition(event.target.value)}/>
+                  <div className="mt-7 my-3">
+                    <label className="text-black">Job Role/job Position</label>
+                    <Input
+                      className="mt-1"
+                      placeholder="Ex. Full stack Developer"
+                      required
+                      onChange={(e) => setJobPosition(e.target.value)}
+                    />
+                  </div>
+                  <div className="my-5">
+                    <label className="text-black">
+                      Job Description/ Tech stack (In Short)
+                    </label>
+                    <Textarea
+                      className="placeholder-opacity-50"
+                      placeholder="Ex. React, Angular, Nodejs, Mysql, Nosql, Python"
+                      required
+                      onChange={(e) => setJobDesc(e.target.value)}
+                    />
+                  </div>
+                  <div className="my-5">
+                    <label className="text-black">Years of Experience</label>
+                    <Input
+                      className="mt-1"
+                      placeholder="Ex. 5"
+                      max="50"
+                      type="number"
+                      required
+                      onChange={(e) => setJobExperience(e.target.value)}
+                    />
+                  </div>
                 </div>
-                <div className='my-3'>
-                    <label>Job Description/ Tech Stack (In Short)</label>
-                    <Textarea placeholder="Ex. React, Angular, NodeJs, MySql etc" required onChange={(event)=>setJobDesc(event.target.value)}/>
-                </div>
-                <div className='my-3'>
-                    <label>Years of Experience</label>
-                    <Input placeholder="Ex. 5" type="number" max="50" required onChange={(event)=>setJobExperience(event.target.value)}/>
-                </div>
-              </div>
-              <div className='flex gap-5 justify-end'>
-                <Button type="button" variant="ghost" onClick={() => setOpenDailog(false)}>Cancel</Button>
-                <Button type="submit" disabled={loading} >
-                  {loading?
-                  <>
-                  <LoaderCircle className='animate-spin'/>'Generating from AI'
-                  </>:'Start Interview'
-                  }
+                <div className="flex gap-5 justify-end">
+                  <Button
+                    type="button"
+                    variant="goast"
+                    onClick={() => setOpenDialog(false)}
+                  >
+                    Cancel
                   </Button>
-              </div>
+                  <Button type="submit" disabled={loading}>
+                    {loading ? (
+                      <>
+                        <LoaderCircle className="animate-spin" />
+                        Generating From AI
+                      </>
+                    ) : (
+                      "Start Interview"
+                    )}
+                  </Button>
+                </div>
               </form>
-          </DialogDescription> 
+            </DialogDescription>
           </DialogHeader>
-      </DialogContent> 
-      </Dialog> 
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+};
 
-    </div> 
-  )
-}
-
-export default Addnewinterview
+export default AddNewInterview;
---
+++ drizzle/0000_early_changeling.sql
@@ -0,0 +1,15 @@
+-- Current sql file was generated after introspecting the database
+-- If you want to run this migration please uncomment this code before executing migrations
+/*
+CREATE TABLE IF NOT EXISTS "mockInterview" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"jsonMockResp" text NOT NULL,
+	"jobPosition" varchar NOT NULL,
+	"jobDesc" varchar NOT NULL,
+	"jobExperience" varchar NOT NULL,
+	"createdBy" varchar NOT NULL,
+	"createdAt" varchar NOT NULL,
+	"mockId" varchar NOT NULL
+);
+
+*/
